2112813605197ba8179176e07a3e66ba41b9d521
Truit Elwell <truitl.elwell@gmail.com>
2024-10-16 13:31:21 -0400

    Added to tiles.js

--- scripts/tiles.js
+++ scripts/tiles.js
@@ -1,7 +1,9 @@
 class Tile {
-    constructor(x, y, rotation = 0, src, bg = false) {
+    constructor(x, y, tx, ty, rotation = 0, src, bg = false) {
         this.x = x
         this.y = y
+        this.tx = tx
+        this.ty = ty
         this.rot = rotation
         this.bg = bg
         this.img = new Image()
@@ -10,7 +12,7 @@ class Tile {
 
     collide(x, y, w, h) {
         if (this.bg) {
-            if (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h){
+            if (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h || x + w > this.x && y + h > this.y){
                 return true
             } else {
                 return false
@@ -21,6 +23,12 @@ class Tile {
     }
 
     draw(ctx) {
-        ctx.drawImage(img, x, y, 32, 32)
+        ctx.drawImage(this.img, this.tx * 32, this.ty * 32, 32, 32, this.x, this.y, 32, 32)
+    }
+}
+
+class Creater extends Tile {
+    constructor (){
+        // TODO
     }
 }
